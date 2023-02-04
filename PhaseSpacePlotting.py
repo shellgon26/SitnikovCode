f82b4a7c57a040130202a47abdab4a32e4c3d61b
Chris Weeks <shellgon26@gmail.com>
2023-02-04 15:24:28 +0000

    Created a function Version for those ho want to use that

--- PhaseSpacePlotting.py
+++ PhaseSpacePlotting.py
@@ -5,19 +5,14 @@ import matplotlib.pyplot as plt
 import math
 #comment time BABY
 
-
 #initial value for z
 z0=0
 
 #initial value for v
 v0=0.5
 
-#initial value for A
-A0=0
-
 #defines the eccentricity of plane orbits
 e=0.1
-
 
 #if you want to see how z and v vary with time, set this to true
 #If you want this graph, I reccomend setting tmax to a single digit number, else
@@ -29,18 +24,95 @@ Plotphasespacegraph=False
 #if you want to see the Poincaré section points, set this to true
 Plotpoincaresection=True
 
-#Set to True if you would like the phase space and the Poincaré section to have the same scale
-ss=True
-
 #Please ensure tmax is an integer
 #tmx is the maximum time to calculate values for (divided by 2pi)
 tmax=1000
+
 #defines how many points to plot per pi
 pointsperhalfcycle=100
+
+
+#Set to True if you would like the phase space and the Poincaré section to have the same scale
+ss=True
 
 #Animates the phase space diagram
 #NOTE: Do not try to close the graph while it is still animating, it will spawn a blank graph in its place, in this case you'll have to close your python to fix it
 Animated = False
+
+
+def Plotwhatyouneed(z0:float,v0:float,e:float,tmax:int,pointsperhalfcycle:int,Plotzvstgraph:list=False,Plotphasespacegraph:bool=False,Plotpoincaresection:bool=False,Animated:bool=False,ss:bool=True):
+    print("hold")
+
+    #initial value for A (It changing doesn't matter so i'm not allowing it to be changed)
+    A0=0
+    #combines initial values into list to be given to odeint function
+    x0=[z0,v0,A0]
+
+    #creates a list of ts to evaluate the ODEs at
+    ts=np.linspace(0,2*tmax*math.pi,2*tmax*pointsperhalfcycle+1)
+
+    #gets the numerical solutions to the ODEs given the inital values
+    #values at all the times within ts
+    x=odeint(odes,x0,ts)
+
+    #initialises a list to store indexes of the points that are multiples of 2pi within
+    checkvals = []
+    #finds and stores all the 2pi indexes
+    for i in range(-1,2*pointsperhalfcycle*tmax,int(2*pointsperhalfcycle)):
+        if i == -1:
+            checkvals.append(0)
+        else:
+            checkvals.append(i)
+
+    if Plotzvtgraph:
+        # #plots z and v on the same axis
+        plt.plot(ts,x[:,0],label="z")
+        plt.plot(ts,x[:,1],label="v")
+
+        #plot labels
+        plt.xlabel("time (s)")
+        #tells you which line is which
+        plt.legend()
+        #display first plot
+        plt.show()
+
+    if Plotphasespacegraph:
+        
+        #If the phase space is being animates, this code will run, which 
+        #replots the data and just adding another point
+        if Animated:
+            fix,ax=plt.subplots()
+            for i in range(0,2000):
+                #if you want to see all the points be animates change the 
+                #2000 to 'len(ts)-1' without the quotation marks
+                ax.plot(x[:,0][:i],x[:,1][:i],"b",linewidth=0.5)
+                plt.xlabel("z")
+                plt.ylabel("v")
+                plt.pause(0.001)
+                ax.clear()
+        #titles the plot with the relevant variables and their values
+        plt.title(f"e={e},z0={z0},v0={v0}, between 0 and {2*tmax}\u03C0")
+        #plots the "full" phase space - the phase space for all the times calculated
+        plt.plot(x[:,0],x[:,1],"b",linewidth=0.5)
+
+    if Plotpoincaresection:
+        #plots poincare section
+        if ss & ~Plotphasespacegraph:
+            #plots an invisible version of the pase space plot to keep the scales the same
+            plt.plot(x[:,0],x[:,1],"w",linewidth=0.5)
+        #titles the graph with the relevant variables and their values
+        plt.title(f"Poincaré section with e={e},z0={z0},v0={v0}, between 0 and {2*tmax}\u03C0")
+        #plots all the points that are at a multiple of 2pi (in t)
+        plt.plot(x[:,0][checkvals],x[:,1][checkvals],"b.")
+
+    #adds labels and displays the plot if it doesn't already exist
+    if ((Plotphasespacegraph& ~Animated) | Plotpoincaresection ):
+        #adds labels to the plot
+        plt.xlabel("z")
+        plt.ylabel("v")
+
+        #display 2nd graph
+        plt.show()
 
 def odes(x,tval):
     #reads in initial values for ode
@@ -60,71 +132,4 @@ def odes(x,tval):
     result =[dz,dv,dA]
     return result
 
-#combines initial values into list to be given to odeint function
-x0=[z0,v0,A0]
-
-#creates a list of ts to evaluate the ODEs at
-ts=np.linspace(0,2*tmax*math.pi,2*tmax*pointsperhalfcycle+1)
-
-#gets the numerical solutions to the ODEs given the inital values
-#values at all the times within ts
-x=odeint(odes,x0,ts)
-
-#initialises a list to store indexes of the points that are multiples of 2pi within
-checkvals = []
-#finds and stores all the 2pi indexes
-for i in range(-1,2*pointsperhalfcycle*tmax,int(2*pointsperhalfcycle)):
-    if i == -1:
-        checkvals.append(0)
-    else:
-        checkvals.append(i)
-
-if Plotzvtgraph:
-    # #plots z and v on the same axis
-    plt.plot(ts,x[:,0],label="z")
-    plt.plot(ts,x[:,1],label="v")
-
-    #plot labels
-    plt.xlabel("time (s)")
-    #tells you which line is which
-    plt.legend()
-    #display first plot
-    plt.show()
-
-if Plotphasespacegraph:
-    
-    #If the phase space is being animates, this code will run, which 
-    #replots the data and just adding another point
-    if Animated:
-        fix,ax=plt.subplots()
-        for i in range(0,2000):
-            #if you want to see all the points be animates change the 
-            #2000 to 'len(ts)-1' without the quotation marks
-            ax.plot(x[:,0][:i],x[:,1][:i],"b",linewidth=0.5)
-            plt.xlabel("z")
-            plt.ylabel("v")
-            plt.pause(0.001)
-            ax.clear()
-    #titles the plot with the relevant variables and their values
-    plt.title(f"e={e},z0={z0},v0={v0}, between 0 and {2*tmax}\u03C0")
-    #plots the "full" phase space - the phase space for all the times calculated
-    plt.plot(x[:,0],x[:,1],"b",linewidth=0.5)
-
-if Plotpoincaresection:
-    #plots poincare section
-    if ss & ~Plotphasespacegraph:
-        #plots an invisible version of the pase space plot to keep the scales the same
-        plt.plot(x[:,0],x[:,1],"w",linewidth=0.5)
-    #titles the graph with the relevant variables and their values
-    plt.title(f"Poincaré section with e={e},z0={z0},v0={v0}, between 0 and {2*tmax}\u03C0")
-    #plots all the points that are at a multiple of 2pi (in t)
-    plt.plot(x[:,0][checkvals],x[:,1][checkvals],"b.")
-
-#adds labels and displays the plot if it doesn't already exist
-if ((Plotphasespacegraph& ~Animated) | Plotpoincaresection ):
-    #adds labels to the plot
-    plt.xlabel("z")
-    plt.ylabel("v")
-
-    #display 2nd graph
-    plt.show()
+Plotwhatyouneed(0.88,0,0.2,1000,1000,Plotpoincaresection=True)
